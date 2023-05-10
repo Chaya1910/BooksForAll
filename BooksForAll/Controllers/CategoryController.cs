8f7e9ea3c2fda9236347536fad4caf7f5af82a71
ChayaShinde <ChayaShinde@FMR-3052-CHAYAS>
2023-05-10 19:50:40 +0530

    Get all Categories from database

--- BooksForAll/Controllers/CategoryController.cs
+++ BooksForAll/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BooksForAll.Data;
+using BooksForAll.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksForAll.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
