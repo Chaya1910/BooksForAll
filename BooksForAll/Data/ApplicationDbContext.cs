83271db509f1c09854a854234e843c1782f59284
ChayaShinde <ChayaShinde@FMR-3052-CHAYAS>
2023-05-10 19:11:55 +0530

    Create Category Table

--- BooksForAll/Data/ApplicationDbContext.cs
+++ BooksForAll/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BooksForAll.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksForAll.Data
 {
@@ -8,5 +9,7 @@ namespace BooksForAll.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
